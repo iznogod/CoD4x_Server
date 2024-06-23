abc40cfeae81aef2fe94f24ce43fad1938e3cebd
IzNoGoD <no@e.mail>
2024-06-23 14:54:29 +0000

    Move more onclientcommand to JH codde

--- src/JH/clientcommand.c
+++ src/JH/clientcommand.c
@@ -1,13 +1,13 @@
 #include "clientcommand.h"
 
-void JH_ClientCommand(scr_entref_t arg);
+void JH_clientcommand_ClientCommand(scr_entref_t arg);
 
 void JH_clientCommand_addMethods()
 {
-    Scr_AddMethod("clientcommand", JH_ClientCommand, 0);
+    Scr_AddMethod("clientcommand", JH_clientcommand_ClientCommand, 0);
 }
 
-void JH_ClientCommand(scr_entref_t arg)
+void JH_clientcommand_ClientCommand(scr_entref_t arg)
 {
     mvabuf;
 
@@ -36,4 +36,42 @@ void JH_ClientCommand(scr_entref_t arg)
             }
         }
     }
+}
+
+void JH_clientcommand_onClientCommand(client_t *client)
+{
+    int callback = script_CallBacks_new[SCR_CB_PLAYERCOMMAND];
+    Scr_MakeArray();
+    int args = SV_Cmd_Argc();
+    for (int i = 0; i < args; i++)
+    {
+        char tmp[1024];
+        SV_Cmd_ArgvBuffer(i, tmp, sizeof(tmp));
+        for (int j = 0; j < strlen(tmp); j++)
+        {
+            if ((unsigned int)tmp[j] < 0x0a)
+            {
+                tmp[j] = '?';
+            }
+        }
+        if (i == 1 && tmp[0] >= 20 && tmp[0] <= 22)
+        {
+            char *part = strtok(tmp + 1, " ");
+            while (part != NULL)
+            {
+                Scr_AddString(part);
+                Scr_AddArray();
+                part = strtok(NULL, " ");
+            }
+        }
+        else
+        {
+            Scr_AddString(tmp);
+            Scr_AddArray();
+        }
+    }
+
+    int threadId = Scr_ExecEntThread(&g_entities[client - svs.clients], callback, 1);
+
+    Scr_FreeThread(threadId);
 }
--- src/JH/clientcommand.h
+++ src/JH/clientcommand.h
@@ -4,5 +4,6 @@
 #include "jh.h"
 
 void JH_clientCommand_addMethods();
+void JH_clientcommand_onClientCommand(client_t *client);
 
 #endif

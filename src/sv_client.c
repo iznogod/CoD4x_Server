15399cc51b4df64f99cf688afc1424c709c63c3c
IzNoGoD <no@e.mail>
2024-06-23 14:53:58 +0000

    Move onclientcommand to JH code

--- src/sv_client.c
+++ src/sv_client.c
@@ -2567,44 +2567,10 @@ void SV_ExecuteClientCommand( client_t *cl, const char *s, qboolean clientOK, qb
 	}
 
 	if ( clientOK ) {
-		// pass unknown strings to the game
+		// pass unknown strings to JH code
 		if ( !u->name && sv.state == SS_GAME )
 		{
-			int callback = script_CallBacks_new[SCR_CB_PLAYERCOMMAND];
-
-			Scr_MakeArray();
-			int args = SV_Cmd_Argc();
-			for(int i = 0; i < args; i++)
-			{
-				char tmp[1024];
-				SV_Cmd_ArgvBuffer(i, tmp, sizeof(tmp));
-				for (int j = 0; j < strlen(tmp); j++)
-				{
-					if ((unsigned int)tmp[j] < 0x0a)
-					{
-						tmp[j] = '?';
-					}
-				}
-				if(i == 1 && tmp[0] >= 20 && tmp[0] <= 22)
-				{
-					char *part = strtok(tmp + 1, " ");
-					while(part != NULL)
-					{
-						Scr_AddString(part);
-						Scr_AddArray();
-						part = strtok(NULL, " ");
-					}
-				}
-				else
-				{
-					Scr_AddString(tmp);
-					Scr_AddArray();
-				}
-			}
-
-			int threadId = Scr_ExecEntThread(&g_entities[cl - svs.clients], callback, 1);
-
-			Scr_FreeThread(threadId);
+			JH_clientcommand_onClientCommand(cl);
 		}
 	}
 
